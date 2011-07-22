5e4c8c0b0287711bbf806a4c045d431fd2858fa3
Eivind Uggedal <eivind@uggedal.com>
2011-07-22 11:23:35 +0200

    Can't override attributes in non-child class. Falling back to exec.

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,13 +1,12 @@
-define zsh::user($ensure=present) {
+define zsh::user($ensure=present, $path="/usr/bin/zsh") {
 
-  User[$name] {
-    shell +> "/usr/bin/zsh",
+  exec { "chsh -s $zsh_path $name":
+    unless => "grep -E '^${name}.+:${$path}$' /etc/passwd",
   }
 
   file { "/home/$name/.zshrc":
     ensure => directory,
     owner => $name,
     group => $name,
-    require => User[$name],
   }
 }
